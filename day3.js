4b7bf8c43f8b23234528fb404136c63118999247
Anshul Saha <anshulsaha15@gmail.com>
2021-04-29 13:34:07 +0530

    day3

---
+++ day3.js
@@ -0,0 +1,63 @@
+// Ctrl + Shift + I (To open console)
+// console.log("js file is connected")
+
+// localStorage.setItem("age", "16")
+// const valueFromLocalStorage = localStorage.getItem("age")
+
+// console.log(valueFromLocalStorage)
+
+// localStorage.clear() // clear local storage
+
+// const myObject = {
+//     "name":"Anshul",
+//     "age":16,
+//     "grade":12
+// }
+
+// const stringObject = JSON.stringify(myObject)
+// console.log(typeof(stringObject))
+// console.log(typeof(JSON.parse(stringObject)))
+
+// for (const key in myObject) {
+//     console.log(key, myObject[key])
+// }
+
+// Object.keys(myObject).forEach(key => {
+//     console.log(key, myObject[key])
+// })
+
+// const myName = "hello world hello world"
+
+// console.log(myName.split("lo"))
+
+// const numbers = [1,2,3,4,5,6] // array
+
+// console.log(numbers)
+
+// console.log(numbers[5])
+
+// numbers.reverse()
+
+// console.log(numbers)
+
+// numbers.forEach(eachNumber => {
+//     console.log(eachNumber+2)
+// })
+
+// function stringToLower(x) {
+//     return x.toLowerCase()
+// }
+
+// function multiply(x,y) {
+//     console.log(x*y)
+// }
+
+// multiply(10,30)
+
+// const resp = null
+
+// if (resp != null) {
+//     console.log("it worked")
+// } else {
+//     console.log("it doesn't work")
+// }
